d3962e0b85ccaab1174d2054d48cd9d6c71728f6
Nik.San.3452 <nik.san.3452@gmail.com>
2022-11-29 11:46:12 +0500

    Добавлен, longpoll и функция загрузки параметров

--- VKinder/main.py
+++ VKinder/main.py
@@ -1,7 +1,20 @@
-from vk_api.longpoll import VkLongPoll, VkEventType
-from random import randrange
-from messages import *
 import requests
 import vk_api
 import data
 import json
+
+from vk_api.longpoll import VkLongPoll, VkEventType
+from random import randrange
+from messages import *
+from config import settings
+
+
+vk = vk_api.VkApi(token=settings.VK_GROUP_TOKEN)
+longpoll = VkLongPoll(vk)
+
+
+def get_params(add_params: dict = None):
+    params = {"access_token": settings.VK_USER_TOKEN, "v": settings.V}
+    if add_params:
+        params.update(add_params)
+    return params
